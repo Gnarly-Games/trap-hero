cf37c1a4e5e4bf37b9f9a0d4516d85f0e4c71313
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2022-08-16 13:40:38 +0300

    feat: end screen

--- Assets/Game/Scripts/UI/FailPanel.cs
+++ Assets/Game/Scripts/UI/FailPanel.cs
@@ -1,8 +1,17 @@
 using Game.Scripts.UI.Base;
+using TMPro;
+using UnityEngine;
 
 namespace Game.Scripts.UI
 {
     public class FailPanel : UIPanel
     {
+        [SerializeField] private TMP_Text killCount;
+
+        public override void OnEnablePanel()
+        {
+            base.OnEnablePanel();
+            killCount.text = $"Kill Count: {SpawnMonitor.Instance.Dead}";
+        }
     }
 }
--- Assets/SpawnMonitor.cs
+++ Assets/SpawnMonitor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Game.Scripts.UI;
 using TMPro;
 using UnityEngine;
 
@@ -9,18 +10,21 @@ public class SpawnMonitor : MonoBehaviour
     // Start is called before the first frame update
     public int Total;
     public int Dead;
+    
     public TMP_Text totalText;
     public TMP_Text spawnRateText;
     public TMP_Text deadCountText;
     public TMP_Text aliveCountText;
+    
  public static SpawnMonitor Instance;
         void Awake() {
             Instance = this;
         }
+        
     internal void RecordDead()
     {
         Dead++;
-        deadCountText.text = "Dead: " +Dead.ToString();
+        deadCountText.text = "Dead: " + Dead.ToString();
     }
 
     internal void RecordSpawn()
